1605c7138c18637b631973ebc18831d96259e94e
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-06 03:39:49 +1000

    Added basic argument parsers to entry points

--- librator/librator.py
+++ librator/librator.py
@@ -1,9 +1,26 @@
-import argparse
+from argparse import ArgumentParser
+from os.path import splitext
 
 
 def main():
-    pass
+    """The primary entry point, can pack or unpack a **Librarian** library."""
+    parser = ArgumentParser(
+        description="Handles Librarian card database packaging.")
+    parser.add_argument('cards', type=str,
+                        help="Directory of single .crd card files")
+    parser.add_argument('library', type=str,
+                        help="Library database file")
+    parser.add_argument('-u', '--unpack', action='store_true',
+                        help="Unpack existing library")
+    args = parser.parse_args()
 
 
 def cardmain():
-    pass
+    """Generate a blank card file."""
+    parser = ArgumentParser(
+        description="Create a black card file template.")
+    parser.add_argument('cardfile', type=str,
+                        help="Card filename")
+    args = parser.parse_args()
+    filename, _ = splitext(args.cardfile)
+    filename += '.crd'
